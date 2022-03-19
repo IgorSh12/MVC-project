44eaff129c77b43da682244b5ec653735aadd434
is42-shishoviya <igorbshihov@mail.ru>
2022-03-19 12:26:07 +0500

    version 1.0.1

--- MVC-Project для GIT/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-const DB_HOSTNAME = 'mysqlis.radiotech.su';
-const DB_USERNAME = 'is342_24_Shishov';
-const DB_PASSWORD = '7US0pkny5q';
-const DB_DATABASE = 'is342_24_Shishov';
-const DB_PORT = '3306'; # Default 3306
--- MVC-Project для GIT/core/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-function view($name, $arg = []) {
-    ob_start();
-    include_once 'views/' . $name . '.php';
-    $content = ob_get_contents();
-    ob_clean();
-    return $content;
-}
