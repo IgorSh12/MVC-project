1528aae4cb635429332117e845b8eba0affc0542
is42-shishoviya <igorbshihov@mail.ru>
2022-03-23 12:21:42 +0500

    Version 1.0.8

--- MVC-Project-main/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-# Подгрузка конфигурации проекта
-include_once 'config.php';
-# Подключаем автозагрузчик
-include_once "autoload.php";
-include_once "core/view.php";
-# Подключаем маршрутизацию
-include_once "route.php";
